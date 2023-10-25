dc3c12f2d6142d3127f8a5cbb8483f545d724711
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-10-25 09:46:12 -0500

    Initial Commit

---
+++ Hackwich Roll/Hackwich_RollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackwich_RollApp.swift
+//  Hackwich Roll
+//
+//  Created by Nicholas Papierniak on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Hackwich_RollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
