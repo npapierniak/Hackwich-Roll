9f17d0adead590edde66b2b133640391f78c4fef
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-10-26 09:50:26 -0500

    Added dice images

--- Hackwich Roll/ContentView.swift
+++ Hackwich Roll/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Dice Roll")
+                .font(.title)
+                .padding()
+            Image ("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: .center)
+                .padding()
+                .onTapGesture {
+                    chooseRandom(times: 3)
+                   
+                    
+                }
+            Spacer()
         }
-        .padding()
+    }
+    
+    func chooseRandom(times:Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+                
+            }
+        }
     }
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
